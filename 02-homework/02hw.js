88aa51525bc8144e5ff1dc5045c2b13ed6bb07eb
Yuliia Kulchytska <yulia.kulchytska98@gmail.com>
2021-09-28 01:18:52 +0300

    added  homework2 with all data

---
+++ 02-homework/02hw.js
@@ -0,0 +1,25 @@
+let numberN = "";
+let numberM = "";
+let shouldIgnoreEvenNumbers;
+
+do {
+    numberN = +prompt("Enter the first number");
+} while (Number.isNaN(numberN));
+console.log(numberN);
+
+do {
+    numberM = +prompt("Enter the second number");
+} while (Number.isNaN(numberM));
+console.log(numberM);
+
+shouldIgnoreEvenNumbers = confirm("Should we skip even numbers?");
+ 
+let sum = 0;
+for(let i = numberN; i<=numberM; i++){
+    if(shouldIgnoreEvenNumbers && i%2===0){
+    }
+    else{
+        sum = sum+i;
+    }
+}
+alert (`result: ${sum}`);
