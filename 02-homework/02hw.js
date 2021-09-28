1c3814ccbd90870b9f9da509cbb9f54349439358
Yuliia Kulchytska <yulia.kulchytska98@gmail.com>
2021-09-29 01:18:40 +0300

    added changes to homework2

--- 02-homework/02hw.js
+++ 02-homework/02hw.js
@@ -1,24 +1,33 @@
-let numberN = "";
-let numberM = "";
+let firstNumber;
+let secondNumber;
 
 do {
-    numberN = +prompt("Enter the first number");
-} while (Number.isNaN(numberN) || numberN == "");
-console.log(numberN);
+    firstTempString = prompt("Enter the first number");
+} while (firstTempString === "" || isNaN(firstTempString));
+firstNumber = parseInt(firstTempString);
+console.log(firstNumber);
 
 do {
-    numberM = +prompt("Enter the second number");
-} while (Number.isNaN(numberM) || numberM == "");
-console.log(numberM);
+    secondTempString = prompt("Enter the second number");
+} while (secondTempString === "" || isNaN(secondTempString));
+secondNumber = parseInt(secondTempString);
+console.log(secondNumber);
 
 const shouldIgnoreEvenNumbers = confirm("Should we skip even numbers?");
+
+if(firstNumber > secondNumber){
+    let tempNumber = firstNumber;
+firstNumber = secondNumber;
+secondNumber = tempNumber;
+}
  
 let sum = 0;
-for(let i = numberN; i<=numberM; i++){
+for(let i = firstNumber; i<=secondNumber; i++){
     if(shouldIgnoreEvenNumbers && i%2===0){
+        continue;
     }
     else{
-        sum = sum+i;
+        sum += i;
     }
 }
 alert (`result: ${sum}`);
