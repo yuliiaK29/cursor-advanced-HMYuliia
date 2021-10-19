e8719437791776b533feac375334082ed721fb6f
Yuliia Kulchytska <yulia.kulchytska98@gmail.com>
2021-10-19 23:14:16 +0300

    added opportunity to replace bad words dynamically

--- 06-homework/06hw.js
+++ 06-homework/06hw.js
@@ -1,7 +1,7 @@
 const getRandomArray = (length, min, max) =>{
     const randomNumbers = [];
     for (let i = 0; i < length ; i++) {
-        randomNumbers.push(Math.round(Math.random() * (max - min) + min));
+        randomNumbers.push(Math.round(Math.random() * (max - min + 1) + min));
 	}
 	return randomNumbers;
 }
@@ -15,10 +15,11 @@ console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2))
 
 const getMedian = (...numbers) => {
     const middleNumber = numbers.filter((number) => Number.isInteger(number)).sort((a, b) => a - b);
+    const halfOfMiddleNumber = middleNumber.length / 2;
     if (middleNumber.length % 2) {
-        return middleNumber[Math.floor(middleNumber.length / 2)];
+        return middleNumber[Math.floor(halfOfMiddleNumber)];
     }
-    return (middleNumber[middleNumber.length / 2] + middleNumber[middleNumber.length / 2 - 1]) / 2;
+    return (middleNumber[halfOfMiddleNumber] + middleNumber[halfOfMiddleNumber - 1]) / 2;
 }
 console.log(getMedian(1, 2, 3, 4, 5));
 
@@ -31,7 +32,18 @@ console.log(countPositiveNumbers(1, -2, 3, -4, -5, 6));
 const getDividedByFive = (...numbers) => numbers.filter(number => number % 5 === 0);
 console.log(getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 
+const badWords = ["fuck", "shit", "bastard", "bollocks"];
 const replaceBadWords = (string) => {
-    return string.split(' ').map((string) => string.replaceAll('fuck', '****').replaceAll('shit', '****')).join(' ');;
+    let tempString = string.split(' ');
+    for (let i = 0; i < badWords.length; i++) {
+        tempString = tempString.map((tempString) => tempString.replaceAll(badWords[i], calculateStars(badWords[i].length)))};
+    return tempString.join(' ');
 }
-console.log(replaceBadWords("It's bullshit!"));
+const calculateStars = (number) => {
+    let starsString = "";
+    for (let i = 0; i < number; i++){
+        starsString +=  "*";
+    }
+    return starsString;
+}
+    console.log(replaceBadWords("It's bullshit,bollocks!"));
---
+++ 07-homework/07hw.js
@@ -0,0 +1,57 @@
+const students = [{
+    name: "Tanya",
+    course: 3,
+    subjects: {
+      math: [4, 4, 3, 4],
+      algorithms: [3, 3, 3, 4, 4, 4],
+      data_science: [5, 5, 3, 4]
+    }
+  }, {
+    name: "Victor",
+    course: 4,
+    subjects: {
+      physics: [5, 5, 5, 3],
+      economics: [2, 3, 3, 3, 3, 5],
+      geometry: [5, 5, 2, 3, 5]
+    }
+  }, {
+    name: "Anton",
+    course: 2,
+    subjects: {
+      statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
+      english: [5, 3],
+      cosmology: [5, 5, 5, 5]
+    }
+  }];
+
+  const getSubjects = (students) => {
+    const subjectForStudent = Object.keys(students.subjects)
+    .map((subject) => subject.substring(0, 1).toUpperCase() + subject.substring(1).toLowerCase().replaceAll('_', ' '));
+    return subjectForStudent;
+  }
+  console.log(getSubjects(students[1]));
+
+  const getAverageMark = (student) => {
+    const marksOfSubjects = Object.values(student.subjects);
+    const marksOfAllSubjects = [].concat(...marksOfSubjects);
+    const averageMark = (marksOfAllSubjects.reduce((a, b) => a + b, 0) / marksOfAllSubjects.length);
+    return +averageMark.toFixed(2);
+  }
+  console.log(getAverageMark(students[0]));
+
+  const getStudentInfo = (students) => {
+    return{course: students.course, name: students.name, averageMark: getAverageMark(students)};
+  }
+  console.log(getStudentInfo(students[0]));
+
+  const getStudentsNames = (students) => {
+    return students.map((students) => students.name).sort();
+  }
+  console.log(getStudentsNames(students));
+
+  const getBestStudent = (students) => {
+    
+  }
+
+
+    
