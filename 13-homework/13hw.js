cce812e5e9aad1e0d4b16944fbca13aed96779c4
Yuliia Kulchytska <yulia.kulchytska98@gmail.com>
2021-11-29 22:16:13 +0200

    correction of mistakes

--- 13-homework/13hw.js
+++ 13-homework/13hw.js
@@ -1,20 +1,17 @@
-const getRandomChinese = function (length){
-return new Promise(function(resolve){
-  let stringOfSymbols = "";
-  let copyOfLength = length;
-    while (copyOfLength > 0) {
+const getRandomChinese = function (length) {
+  return new Promise(function (resolve) {
+    let stringOfSymbols = "";
+    let i = 1;
+    while (i <= length) {
       setTimeout(() => {
-      const timeInMs = Date.now();
-      const lastFiveCharacters = timeInMs.toString().slice(-5);
-      const sign = parseInt(lastFiveCharacters);
-      const symbol = String.fromCharCode(sign);
-      stringOfSymbols += symbol;
-      if (stringOfSymbols.length === length) {
-        resolve(stringOfSymbols);
-    }
-    }, 50);
-    copyOfLength--;
+        const symbol = String.fromCharCode(Date.now()).slice(-5);
+        stringOfSymbols += symbol;
+        if (stringOfSymbols.length === length) {
+          resolve(stringOfSymbols);
+        }
+      }, i * 50);
+      i++;
     }
   });
-}
+};
 getRandomChinese(4).then(result => console.log(result));
